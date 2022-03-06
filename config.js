f84f37e42776a3dac9404c6eb46d6a359164496e
TinhCm <66770122+TinhCm@users.noreply.github.com>
2022-03-07 03:23:35 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,9 @@
+const nameGirl = 'Thương';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Chúc Mừng 8/3';
+const titleCard = 'Tặng  ❤';
+const contentCard = 'Chúc chị có một ngày 8/3 thật nhiều niềm vui và may mắn.';
+
+const giftImage = 'hoaithuong.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
