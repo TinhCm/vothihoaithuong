390406fb1d8e9a3e08ea60799ac3fe7dbf230b99
TinhCm <66770122+TinhCm@users.noreply.github.com>
2022-03-07 03:59:50 +0700

    f

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
-const nameGirl = 'Thương';
+const nameGirl = 'Hoài Thương';
 const giftUrl = 'http://nodemy.vn';
 const eventName = 'Chúc Mừng 8/3';
-const titleCard = 'Tặng  ❤';
-const contentCard = 'Chúc chị có một ngày 8/3 thật nhiều niềm vui và may mắn.';
+const titleCard = 'Tặng ❤';
+const contentCard = 'Chúc chị có một ngày 8/3 thật nhiều niềm vui và may mắn, nụ cười luôn nỡ trên môi và đặc biệt là ngày càng xinh đẹp nha ❤';
 
 const giftImage = 'hoaithuong.jpg';
 const base64 = '';
