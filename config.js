d62c3eec1b7dc8b4bb774fade99d43ebc2a04253
TinhCm <66770122+TinhCm@users.noreply.github.com>
2022-03-07 22:42:55 +0700

    f

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const nameGirl = 'Hoài Thương';
-const giftUrl = 'https://drive.google.com/file/d/1fdf1YC5_WPzT81b7I5Bt7dieqoFAeD0B/view?usp=sharing';
+const giftUrl = 'https://youtu.be/HV8aE3IyD-Y';
 const eventName = 'Chúc Mừng 8/3';
 const titleCard = 'Tặng ❤';
 const contentCard = 'Chúc chị có một ngày 8/3 thật nhiều niềm vui và may mắn, nụ cười luôn nỡ trên môi và đặc biệt là ngày càng xinh đẹp nha ❤';
