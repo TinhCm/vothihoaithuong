74aab58f9f5cf4537243809046599bfbde3050e9
TinhCm <66770122+TinhCm@users.noreply.github.com>
2022-06-24 22:08:48 +0700

    cap nhat

--- config.js
+++
@@ -1,9 +0,0 @@
-const nameGirl = 'Hoài Thương';
-const giftUrl = 'https://youtu.be/HV8aE3IyD-Y';
-const eventName = 'Chúc Mừng 8/3';
-const titleCard = 'Tặng ❤';
-const contentCard = 'Chúc chị có một ngày 8/3 thật nhiều niềm vui và may mắn, nụ cười luôn nỡ trên môi và đặc biệt là ngày càng xinh đẹp nha ❤';
-
-const giftImage = 'hoaithuong.jpg';
-const base64 = '';
-const giftImageBase64 = "data:image/png;base64, " + base64;
